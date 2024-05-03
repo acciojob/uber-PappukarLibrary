59879d97bcae0a997b7a4b1920fe9a42a633906d
Pappu Kar <kar46406@gmail.com>
2024-05-03 22:46:43 +0530

    uber

--- src/main/java/com/driver/repository/TripBookingRepository.java
+++ src/main/java/com/driver/repository/TripBookingRepository.java
@@ -1,4 +1,7 @@
+
+
 package com.driver.repository;
+import com.driver.model.TripBooking;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
